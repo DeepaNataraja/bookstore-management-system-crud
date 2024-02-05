4cd9a094c372f970c9401f301919a338f56cab96
DeepaNatarajan <103093154+DeepaNataraja@users.noreply.github.com>
2024-02-05 12:10:52 +0530

    routes.py

--- routes.py
+++ routes.py
@@ -3,22 +3,34 @@ from service import View,Insert,Update,Delete
 
 crud_blueprint = Blueprint('crud',__name__)
 
+from flask import Blueprint
+from Books.service import view,delete,update
+
+
+crud_blueprint = Blueprint('crud',__name__)
+
 @crud_blueprint.route('/view', methods=['GET'])
+
 def Get():
-    data = View()
+    data = view()
     return data
 
-@crud_blueprint.route('/insert', methods=['POST'])
-def Post():
-    data = Insert()
+# @crud_blueprint.add_url_rule(rule='/view',
+#                           endpoint='view',
+#                           view_func=View,
+#                           methods=['GET'])
+
+@crud_blueprint.route('/delete', methods=['DELETE'])
+def Del():
+    data = delete() 
     return data
 
 @crud_blueprint.route('/update', methods=['PUT'])
 def Updatee():
-    data = Update()
+    data = update()
     return data
 
-@crud_blueprint.route('/delete', methods=['DELETE'])
-def Del():
-    data = Delete() 
-    return data
+# @crud_blueprint.route('/insert', methods=['POST'])
+# def Post():
+#     data = Insert()
+#     return data
